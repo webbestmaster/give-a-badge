a23cc01f0926f1654b7adc0682868d19865a9d92
webBestMaster <web.best.master@gmail.com>
2018-09-06 22:09:00 +0300

    refactor

--- www/js/app/index.js
+++ www/js/app/index.js
@@ -4,6 +4,8 @@ import type {Node} from 'react';
 import React from 'react';
 import Route from 'react-router-dom/Route';
 import Switch from 'react-router-dom/Switch';
+import muiTheme from '../component/ui/mui-theme';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import System from '../component/system';
 import Auth from '../component/auth';
 
@@ -11,19 +13,26 @@ import Home from '../page/home';
 import routes from './routes';
 import BrowserRouter from 'react-router-dom/BrowserRouter';
 import NotFound from '../component/not-found';
+import ReduxStoreProvider from '../app-provider';
 
-export default function App(): Array<Node> {
-    return [
-        <Auth key="auth"/>,
-        <System key="system">
-            <BrowserRouter>
-                <Switch>
-                    <Route component={Home} path={routes.index.index} exact/>
-                    <Route component={Home} path={routes.index.badgeCategoryList} exact/>
-                    <Route component={Home} path={routes.index.giveTheBadge} exact/>
-                    <Route component={NotFound}/>
-                </Switch>
-            </BrowserRouter>
-        </System>
-    ];
+export default function App(): Node {
+    return (
+        /* eslint-disable react/jsx-max-depth */
+        <ReduxStoreProvider>
+            <MuiThemeProvider theme={muiTheme}>
+                <Auth key="auth"/>
+                <System key="system">
+                    <BrowserRouter>
+                        <Switch>
+                            <Route component={Home} path={routes.index.index} exact/>
+                            <Route component={Home} path={routes.index.badgeCategoryList} exact/>
+                            <Route component={Home} path={routes.index.giveTheBadge} exact/>
+                            <Route component={NotFound}/>
+                        </Switch>
+                    </BrowserRouter>
+                </System>
+            </MuiThemeProvider>
+        </ReduxStoreProvider>
+        /* eslint-enable react/jsx-max-depth */
+    );
 }
--- www/js/index.js
+++ www/js/index.js
@@ -3,37 +3,9 @@
 
 import React from 'react';
 import {render} from 'react-dom';
-import {Provider} from 'react-redux';
-import thunk from 'redux-thunk';
-import {applyMiddleware, combineReducers, createStore} from 'redux';
-import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
-import muiTheme from './component/ui/mui-theme';
-import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
-
 import App from './app';
 
-import * as reducers from './app-reducer';
-
-const reducer = combineReducers({
-    ...reducers
-});
-
-const composeEnhancers = composeWithDevTools({
-    // options like actionSanitizer, stateSanitizer
-});
-
-const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
-
-/* eslint-disable react/jsx-max-depth */
-render(
-    <Provider store={store}>
-        <MuiThemeProvider theme={muiTheme}>
-            <App/>
-        </MuiThemeProvider>
-    </Provider>,
-    window.document.querySelector('.js-app-wrapper')
-);
-/* eslint-enable react/jsx-max-depth */
+render(<App/>, window.document.querySelector('.js-app-wrapper'));
 
 // check build
 // eslint-disable-next-line id-match
