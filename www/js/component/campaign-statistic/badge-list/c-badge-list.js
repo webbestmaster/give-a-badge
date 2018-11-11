ca81d68dc3a7370b6a923995ef0f74ab9c570f56
turovtsov <dturovtsov@marathonbet.ru>
2018-11-11 19:04:49 +0300

    comments - in progress...

--- www/js/component/campaign-statistic/badge-list/c-badge-list.js
+++ www/js/component/campaign-statistic/badge-list/c-badge-list.js
@@ -41,23 +41,94 @@ export class BadgeList extends Component<PropsType, StateType> {
         const {props} = view;
         const {campaignStatisticDataList} = props;
 
-        return campaignStatisticDataList.map(
-            (campaignStatisticData: CampaignStatisticDataType): DataType => {
-                return campaignStatisticData.badge;
+        const badgeList = [];
+
+        campaignStatisticDataList.forEach((campaignStatisticData: CampaignStatisticDataType) => {
+            const {badge} = campaignStatisticData;
+
+            const hasBadgeInList = badgeList.some((badgeInList: DataType): boolean => badgeInList.id === badge.id);
+
+            if (hasBadgeInList) {
+                return;
             }
-        );
+
+            badgeList.push(badge);
+        });
+
+        if (badgeList.length > 0) {
+            view.activateBadge(badgeList[0].id);
+        }
+
+        return badgeList;
     }
 
-    renderBadge = (badgeData: DataType, index: number): Node => {
+    activateBadge(badgeId: string | number) {
         const view = this;
         const {state, props} = view;
         const {selectedBadgeIdList} = state;
+
+        if (selectedBadgeIdList.includes(badgeId)) {
+            console.log('selectedBadgeIdList already include the', badgeId);
+            return;
+        }
+
+        view.setState({selectedBadgeIdList: [...selectedBadgeIdList, badgeId]});
+    }
+
+    deactivateBadge(badgeId: string | number) {
+        const view = this;
+        const {state, props} = view;
+        const {selectedBadgeIdList} = state;
+
+        if (!selectedBadgeIdList.includes(badgeId)) {
+            console.log('selectedBadgeIdList already NOT include the', badgeId);
+            return;
+        }
+
+        if (selectedBadgeIdList.length <= 1) {
+            console.log('selectedBadgeIdList too short to deactivateBadge', badgeId);
+            return;
+        }
+
+        selectedBadgeIdList.splice(selectedBadgeIdList.indexOf(badgeId), 1);
+
+        view.setState({selectedBadgeIdList: [...selectedBadgeIdList]});
+    }
+
+    toggleBadgeActive(badgeId: string | number) {
+        const view = this;
+        const {state, props} = view;
+        const {selectedBadgeIdList} = state;
+
+        if (selectedBadgeIdList.includes(badgeId)) {
+            view.deactivateBadge(badgeId);
+            return;
+        }
+
+        view.activateBadge(badgeId);
+    }
+
+    createOnClickHandler(badgeId: string | number): () => void {
+        return () => {
+            const view = this;
+
+            view.toggleBadgeActive(badgeId);
+        };
+    }
+
+    renderBadge = (badgeData: DataType): Node => {
+        const view = this;
+        const {state, props} = view;
+        const {selectedBadgeIdList} = state;
+        const handleOnClick = view.createOnClickHandler(badgeData.id);
 
         return (
             <button
                 title={badgeData.name}
                 type="button"
-                key={`${index}/${badgeData.id}`}
+                key={badgeData.id}
+                onClick={handleOnClick}
+                onKeyPress={handleOnClick}
                 className={classNames(style.badge_tem, {
                     [style.badge_tem__selected]: selectedBadgeIdList.includes(badgeData.id),
                 })}
