cb4c572c31c6c02a5b85fcd70b5bd5f0e714363d
turovtsov <dturovtsov@marathonbet.ru>
2018-11-11 17:20:46 +0300

    comments - in progress...

--- www/js/component/campaign-statistic/comment-list/c-comment-list.js
+++ www/js/component/campaign-statistic/comment-list/c-comment-list.js
@@ -8,7 +8,8 @@ import React, {Component, Fragment} from 'react';
 import style from './style.scss';
 import {Scroll} from '../../ui/scroll/c-scroll';
 import {Locale} from '../../locale/c-locale';
-import type {CampaignStatisticDataListType} from '../api';
+import type {CampaignStatisticDataListType, CampaignStatisticDataType, DataType} from '../api';
+import classNames from 'classnames';
 
 type PassedPropsType = {|
     +campaignStatisticDataList: CampaignStatisticDataListType,
@@ -16,7 +17,6 @@ type PassedPropsType = {|
 
 type PropsType = $Exact<{
     ...$Exact<PassedPropsType>,
-    // ...$Exact<ContextRouterType>
     +children: Node,
 }>;
 
@@ -38,30 +38,39 @@ export class CommentList extends Component<PropsType, StateType> {
         };
     }
 
+    getCommentList(): Array<DataType> {
+        const view = this;
+        const {props} = view;
+        const {campaignStatisticDataList} = props;
+
+        return campaignStatisticDataList.map(
+            (campaignStatisticData: CampaignStatisticDataType): DataType => {
+                return campaignStatisticData.badge;
+            }
+        );
+    }
+
+    renderComment = (badgeData: DataType, index: number): Node => {
+        return (
+            <div title={badgeData.name} key={`${index}/${badgeData.id}`} className={classNames(style.comment_item)}>
+                comment
+            </div>
+        );
+    };
+
     render(): Node {
+        const view = this;
+
+        const commentList = view.getCommentList();
+
         return (
             <div className={style.comment_list}>
                 <h3 className={style.comment_list__header}>
                     <Locale stringKey="CAMPAIGN__STATISTIC__COMMENT"/>
                 </h3>
                 <div className={style.comment_list_scroll}>
-                    <Scroll slideWidth={1000} direction="horizontal">
-                        <div>
-                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad assumenda dolorum eligendi
-                            molestiae possimus quia quod veniam veritatis voluptatem? A autem ea itaque molestiae
-                            pariatur porro possimus reprehenderit tenetur voluptatem? Lorem ipsum dolor sit amet,
-                            consectetur adipisicing elit. Ad assumenda dolorum eligendi molestiae possimus quia quod
-                            veniam veritatis voluptatem? A autem ea itaque molestiae pariatur porro possimus
-                            reprehenderit tenetur voluptatem? Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                            Ad assumenda dolorum eligendi molestiae possimus quia quod veniam veritatis voluptatem? A
-                            autem ea itaque molestiae pariatur porro possimus reprehenderit tenetur voluptatem? Lorem
-                            ipsum dolor sit amet, consectetur adipisicing elit. Ad assumenda dolorum eligendi molestiae
-                            possimus quia quod veniam veritatis voluptatem? A autem ea itaque molestiae pariatur porro
-                            possimus reprehenderit tenetur voluptatem? Lorem ipsum dolor sit amet, consectetur
-                            adipisicing elit. Ad assumenda dolorum eligendi molestiae possimus quia quod veniam
-                            veritatis voluptatem? A autem ea itaque molestiae pariatur porro possimus reprehenderit
-                            tenetur voluptatem?
-                        </div>
+                    <Scroll slideWidth={commentList.length * 465} direction="horizontal">
+                        <div className={style.comment_list_wrapper}>{commentList.map(view.renderComment)}</div>
                     </Scroll>
                 </div>
             </div>
